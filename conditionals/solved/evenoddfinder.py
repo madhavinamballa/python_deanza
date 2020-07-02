ce5c0cba06c08f44294bb11f1e335eb759bf83ce
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2020-07-02 13:31:48 -0700

    made changes

--- conditionals/solved/evenoddfinder.py
+++ conditionals/solved/evenoddfinder.py
@@ -1,6 +1,1 @@
 #python program to check if a Number is even or odd
-number=int(input("hey please enter your number: "))
-if number%2==0:
-    print("entered number is an even number")
-else:
-    print("entered number is odd number")
--- inputs/solved/input_1.py
+++
@@ -1,4 +0,0 @@
-name=input("what is your name: ")
-print(name)
-grade=int(input("please enter your grade")
-print(type(grade))
